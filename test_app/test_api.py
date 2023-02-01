18b213829979fb2dd3e9d3b35cab049e10ea0932
youssef <youssef.h.alghamry@gmail.com>
2023-02-01 12:00:00 +0000

    d

--- test_app/test_api.py
+++ test_app/test_api.py
@@ -1,17 +1,13 @@
 import unittest
-from app import sum, mult  
+from src.app import sum, mult
 
 class TestMathFunctions(unittest.TestCase):
 
     def test_sum(self):
         self.assertEqual(sum(2, 3), 5)
-        self.assertEqual(sum(-1, 1), 0)
-        self.assertEqual(sum(0, 0), 0)
 
     def test_mult(self):
-        self.assertEqual(mult(2, 3), 6)
-        self.assertEqual(mult(-1, 5), -5)
-        self.assertEqual(mult(0, 10), 0)
+        self.assertEqual(mult(2, 4), 8)
 
 if __name__ == '__main__':
     unittest.main()
