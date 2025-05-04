e3fc9780e5c330de50578bf2320b38211acdc453
Heba <heba.atef.muhammed@gmail.com>
2025-05-04 15:49:33 +0300

    initial model training

---
+++ src/train.py
@@ -0,0 +1,145 @@
+"""
+This module contains functions to preprocess and train the model
+for bank consumer churn prediction.
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.utils import resample
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+    ConfusionMatrixDisplay,
+)
+
+
+def rebalance(data):
+    """
+    Resample data to keep balance between target classes.
+
+    The function uses the resample function to downsample the majority class to match the minority class.
+
+    Args:
+        data (pd.DataFrame): DataFrame
+
+    Returns:
+        pd.DataFrame): balanced DataFrame
+    """
+    churn_0 = data[data["Exited"] == 0]
+    churn_1 = data[data["Exited"] == 1]
+    if len(churn_0) > len(churn_1):
+        churn_maj = churn_0
+        churn_min = churn_1
+    else:
+        churn_maj = churn_1
+        churn_min = churn_0
+    churn_maj_downsample = resample(
+        churn_maj, n_samples=len(churn_min), replace=False, random_state=1234
+    )
+
+    return pd.concat([churn_maj_downsample, churn_min])
+
+
+def preprocess(df):
+    """
+    Preprocess and split data into training and test sets.
+
+    Args:
+        df (pd.DataFrame): DataFrame with features and target variables
+
+    Returns:
+        ColumnTransformer: ColumnTransformer with scalers and encoders
+        pd.DataFrame: training set with transformed features
+        pd.DataFrame: test set with transformed features
+        pd.Series: training set target
+        pd.Series: test set target
+    """
+    filter_feat = [
+        "CreditScore",
+        "Geography",
+        "Gender",
+        "Age",
+        "Tenure",
+        "Balance",
+        "NumOfProducts",
+        "HasCrCard",
+        "IsActiveMember",
+        "EstimatedSalary",
+        "Exited",
+    ]
+    cat_cols = ["Geography", "Gender"]
+    num_cols = [
+        "CreditScore",
+        "Age",
+        "Tenure",
+        "Balance",
+        "NumOfProducts",
+        "HasCrCard",
+        "IsActiveMember",
+        "EstimatedSalary",
+    ]
+    data = df.loc[:, filter_feat]
+    data_bal = rebalance(data=data)
+    X = data_bal.drop("Exited", axis=1)
+    y = data_bal["Exited"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=1912
+    )
+    col_transf = make_column_transformer(
+        (OneHotEncoder(handle_unknown="ignore", drop="first"), cat_cols),
+        remainder="passthrough",
+    )
+
+    X_train = col_transf.fit_transform(X_train)
+    X_train = pd.DataFrame(X_train, columns=col_transf.get_feature_names_out())
+
+    X_test = col_transf.transform(X_test)
+    X_test = pd.DataFrame(X_test, columns=col_transf.get_feature_names_out())
+
+    return col_transf, X_train, X_test, y_train, y_test
+
+
+def train(X_train, y_train):
+    """
+    Train a logistic regression model.
+
+    Args:
+        X_train (pd.DataFrame): DataFrame with features
+        y_train (pd.Series): Series with target
+
+    Returns:
+        LogisticRegression: trained logistic regression model
+    """
+    log_reg = LogisticRegression(max_iter=1000)
+    log_reg.fit(X_train, y_train)
+    return log_reg
+
+
+def main():
+    df = pd.read_csv("data/Churn_Modelling.csv")
+    col_transf, X_train, X_test, y_train, y_test = preprocess(df)
+    model = train(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print(f"Accuracy score: {accuracy_score(y_test, y_pred):.2f}")
+    print(f"Precision score: {precision_score(y_test, y_pred):.2f}")
+    print(f"Recall score: {recall_score(y_test, y_pred):.2f}")
+    print(f"F1 score: {f1_score(y_test, y_pred):.2f}")
+
+    conf_mat = confusion_matrix(y_test, y_pred, labels=model.classes_)
+    conf_mat_disp = ConfusionMatrixDisplay(
+        confusion_matrix=conf_mat, display_labels=model.classes_
+    )
+    conf_mat_disp.plot()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
